7b57bcc4c8c0ef0e4f828e0e0d65f34f87079c85
ShaanHossain <shaanhossain19@gmail.com>
2021-12-08 14:28:59 -0500

    LOLOFASDHSAD

---
+++ fuck_me.py
@@ -0,0 +1,280 @@
+#import files
+import sys
+from csv import reader
+from typing import List
+from nltk.tokenize import RegexpTokenizer
+from gensim.parsing.preprocessing import remove_stopwords, strip_punctuation
+import numpy as np
+import inflect
+from nltk.stem import LancasterStemmer
+from nltk.stem import WordNetLemmatizer
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+from keras.models import Sequential
+from keras.layers import Dropout, Dense, Embedding, LSTM, Bidirectional
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+print("Starting Neural Language System")
+
+# Determines which dataset use and how much to use :
+# HateSpeech: Column-0 : Sentence, Column-1 : Label [noHate-0, Hate-1]
+# either 'HateSpeech' or 'KaggleTwitter' or 'TDavidson'
+dataset_to_use = "HateSpeech"
+dataset_percentage = 100  # percentage range 1 to 100
+
+# Initializes file path, column of csv file to parse and
+# the delimiter for parsing
+training_file = ""
+test_file = ""
+sentence_column_to_parse = None
+label_column_to_parse = None
+lancaster = LancasterStemmer()
+wordnet_lemmatizer = WordNetLemmatizer()
+delimiter = ","
+if dataset_to_use == "HateSpeech":
+    training_file = "datasets/hate-speech/train.txt"
+    test_file = "datasets/hate-speech/test.txt"
+    delimiter = "\t"
+    sentence_column_to_parse = 0
+    label_column_to_parse = 1
+elif dataset_to_use == "KaggleTwitter":
+    training_file = "datasets/kaggle-twitter/train.csv"
+    test_file = "datasets/kaggle-twitter/test.csv"
+    sentence_column_to_parse = 2
+    label_column_to_parse = 1
+elif dataset_to_use == "TDavidson":
+    training_file = "datasets/t-davidson-hate-speech/labeled_data.csv"
+    # TODO: Update test path for this dataset
+    # test_file = "datasets/kaggle-twitter/test.csv"
+    sentence_column_to_parse = 6
+    label_column_to_parse = 2
+else:
+    print("Invalid Dataset specified")
+    sys.exit(1)
+
+print(dataset_to_use + " Dataset Loaded")
+
+def replace_numbers(sentence:List[str]) -> List[str]:
+    """Replace all interger occurrences in list of tokenized words
+    with textual representation"""
+    p = inflect.engine()
+    new_words = []  
+    for word in sentence.split():
+        if word.isdigit():
+            new_word = p.number_to_words(word)
+            new_words.append(new_word)
+        else:
+            new_words.append(word)
+    return " ".join(new_words)
+
+def stem_words(sentence: List[str]) -> List[str]:
+    """Stems the given sentence
+
+    Args:
+        sentence (list): words to be stemmed
+
+    Returns:
+        str: stemmed sentence
+    """
+    stemmed_words = []
+    for word in sentence.split():
+        stemmed_words.append(lancaster.stem(word))
+    return " ".join(stemmed_words)
+
+def preprocessing(running_lines: List[str]) -> List[str]:
+    """This function takes in the running test and return back the
+    preprocessed text. Six tasks are done as part of this:
+      1. lower word case
+      2. remove stopwords
+      3. remove punctuation
+      4. convert numbers to texts
+      5. perform stemming
+
+    Args:
+        sentence (List[str]): list of lines
+
+    Returns:
+        List[str]: list of sentences which are processed
+    """
+    preprocessed_lines = []
+    tokenizer = RegexpTokenizer(r"\w+")
+    for line in running_lines:
+        # lower case
+        lower_case_data = line.lower()
+        # remove stop words
+        data_without_stop_word = remove_stopwords(lower_case_data)
+        # remove punctunation
+        data_without_punct = strip_punctuation(data_without_stop_word)
+        # replace numbers '1' to 'one'
+        processed_data = replace_numbers(data_without_punct)
+        # stem words
+        processed_data = stem_words(processed_data)
+        # add start and stop tags
+        # processed_data.insert(0, "<s>")
+        # processed_data.append("</s>")
+        preprocessed_lines.append(processed_data)
+    return preprocessed_lines
+
+def parse_data(training_file_path: str, percentage: int,
+               sentence_column: int, label_column: int,
+               delimit: str):
+    """This function is used to parse input lines
+    and returns a the provided percent of data.
+
+    Args:
+        lines (List[str]): list of lines
+        percentage (int): percent of the dataset needed
+        sentence_column (int): sentence column from the dataset
+        label_column (int): label column from the dataset
+        delimit (str): delimiter
+    Returns:
+        List[str], List[str]: examples , labels -> [percentage of dataset]
+    """
+    percentage_sentences = []
+    percentage_labels = []
+    with open(training_file_path, "r", encoding="utf8",
+              errors="ignore") as csvfile:
+        read_sentences = []
+        label_sentences = []
+        csv_reader = reader(csvfile, delimiter=delimit)
+        # skipping header
+        header = next(csv_reader)
+        # line_length = len(list(csv_reader_copy))
+        if header is not None:
+            for row in csv_reader:
+                read_sentences.append(row[sentence_column])
+                label_sentences.append(int(row[label_column]))
+        end_of_data = int(len(read_sentences) * percentage * .01)
+        percentage_sentences = read_sentences[0:end_of_data]
+        percentage_labels = label_sentences[0:end_of_data]
+    return percentage_sentences, percentage_labels
+
+print("Parsing Data")
+
+train_sentences, train_labels = parse_data(training_file,
+                                           dataset_percentage,
+                                           sentence_column_to_parse,
+                                           label_column_to_parse,
+                                           delimiter)
+# parse and preprocess the data
+
+print("Preprocessing Data")
+
+processed_train_sentences = preprocessing(train_sentences)
+# verify the processed sentences
+# for sentence in sentences:
+#     print(sentence)
+# This is the baseline classifier
+print(
+    f"Performing Improved - BiLSTM on {dataset_to_use}"
+    f" with {dataset_percentage} % data ")
+
+def convert_sentence_word_embeddings(X_train_sentences:List[str]):
+    """Converts the sentences into word embeddings.
+
+    Args:
+        X_train_sentences (List[str]): list of training sentences
+
+    Returns:
+        tuple: word embeddings for each sentence, vocab size and embedding dictionary
+    """
+    tokenizer = Tokenizer()
+    text = np.array(X_train_sentences)
+    tokenizer.fit_on_texts(X_train_sentences)
+    # pickle.dump(tokenizer, open('text_tokenizer.pkl', 'wb'))
+    # Uncomment above line to save the tokenizer as .pkl file 
+    sequences = tokenizer.texts_to_sequences(text)
+    word_index = tokenizer.word_index
+    text = pad_sequences(sequences)
+    print('Found %s unique tokens.' % len(word_index))
+    indices = np.arange(text.shape[0])
+    # np.random.shuffle(indices)
+    text = text[indices]
+    embeddings_dict = {}
+    file_embeddings = open("utils/glove.twitter.27B.50d.txt", encoding="utf8")
+    for embedding_line in file_embeddings:
+        values = embedding_line.split()
+        word = values[0]
+        coefs = np.asarray(values[1:], dtype='float32')
+        embeddings_dict[word] = coefs
+    file_embeddings.close()
+    print('Total %s word vectors.' % len(embeddings_dict))
+    return (text, word_index, embeddings_dict)
+
+print("Converting sentences to word embeddings")
+
+X_train_Glove_s, word_index_s, embeddings_dict_s = convert_sentence_word_embeddings(processed_train_sentences)
+embedding_size = len(X_train_Glove_s[0])
+## Check function
+# x_train_sample = ["Lorem Ipsum is simply dummy text of the printing and typesetting industry", "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout"]
+# X_train_Glove_s, word_index_s, embeddings_dict_s = convert_sentence_word_embeddings(x_train_sample)
+# print("\n X_train_Glove_s \n ", X_train_Glove_s)
+# print("\n Word index of the word testing is : ", word_index_s["industry"])
+# print("\n Embedding for thw word want \n \n", embeddings_dict_s["want"])
+
+def build_bilstm(word_index, embeddings_dict, nclasses,  MAX_SEQUENCE_LENGTH, EMBEDDING_DIM=50, dropout=0.5, hidden_layer = 3, lstm_node = 32):
+    model = Sequential()
+    # Make the embedding matrix using the embedding_dict
+    embedding_matrix = np.random.random((len(word_index) + 1, EMBEDDING_DIM))
+    for word, i in word_index.items():
+        embedding_vector = embeddings_dict.get(word)
+        if embedding_vector is not None:
+            # words not found in embedding index will be all-zeros.
+            if len(embedding_matrix[i]) != len(embedding_vector):
+                print("could not broadcast input array from shape", str(len(embedding_matrix[i])),
+                      "into shape", str(len(embedding_vector)), " Please make sure your"
+                                                                " EMBEDDING_DIM is equal to embedding_vector file ,GloVe,")
+                exit(1)
+            embedding_matrix[i] = embedding_vector
+            
+    # Add embedding layer
+    model.add(Embedding(len(word_index) + 1,
+                                EMBEDDING_DIM,
+                                weights=[embedding_matrix],
+                                input_length=MAX_SEQUENCE_LENGTH,
+                                trainable=True))
+    # Add hidden layers 
+    for i in range(0,hidden_layer):
+        # Add a bidirectional lstm layer
+        model.add(Bidirectional(LSTM(lstm_node, return_sequences=True, recurrent_dropout=0.2)))
+        # Add a dropout layer after each lstm layer
+        model.add(Dropout(dropout))
+    model.add(Bidirectional(LSTM(lstm_node, recurrent_dropout=0.2)))
+    model.add(Dropout(dropout))
+    # Add the fully connected layer with 256 nurons and relu activation
+    model.add(Dense(256, activation='relu'))
+    # Add the output layer with softmax activation since we have 2 classes
+    model.add(Dense(nclasses, activation='softmax'))
+    # Compile the model using sparse_categorical_crossentropy
+    model.compile(loss='sparse_categorical_crossentropy',
+                      optimizer='adam',
+                      metrics=['accuracy'])
+    return model
+
+def plot_graphs(axs, graph_index, history, string):
+  axs[graph_index].plot(history.history[string])
+  axs[graph_index].plot(history.history['val_'+string], '')
+  axs[graph_index].xlabel("Epochs")
+  axs[graph_index].ylabel(string)
+  axs[graph_index].legend([string, 'val_'+string])
+  axs[graph_index].show()
+
+print("Building Model!")
+model = build_bilstm(word_index_s, embeddings_dict_s, 2, embedding_size)
+model.summary()
+
+print("Training the model")
+
+X_train, X_test, y_train, y_test = train_test_split(X_train_Glove_s, train_labels, test_size = 0.2)
+data_history = model.fit(np.array(X_train), np.array(y_train),
+                           validation_data=(np.array(X_test),np.array(y_test)),
+                           epochs=20,
+                           batch_size=128,
+                           verbose=1)
+
+fig, axs = plt.subplots(2)
+
+plot_graphs(axs, 0, data_history, 'accuracy')
+plot_graphs(axs, 1, data_history, 'loss')
--- neural_network.py
+++ neural_network.py
@@ -176,3 +176,174 @@ y_pred_tanh_range = converge_to_binary(y_pred_numpy)
 # Plotting the test data
 # plot_data(x_test, y_pred_tanh_range)
 
+# Stuff I want to save:
+
+## Check function
+x_train_sample = ["Lorem Ipsum is simply dummy text of the printing and typesetting industry", "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout"]
+# X_train_Glove_s, word_index_s, embeddings_dict_s = convert_sentence_word_embeddings(x_train_sample)
+# print("\n X_train_Glove_s \n ", X_train_Glove_s)
+print("\n Word index of the word testing is : ", word_index_s["industry"])
+print("\n Embedding for the word want \n \n", embeddings_dict_s["want"])
+len(embeddings_dict_s["want"])
+
+word_index_to_embedding = {}
+
+# print(X_train_Glove_s[0])
+
+# for i in embeddings_dict_s.keys():
+#     print(i)
+#     print(word_index_s[i])
+#     word_index_to_embedding[word_index_s[i]] =  embeddings_dict_s[i]
+
+# print(word_index_to_embedding[0])
+
+# print(train_labels)
+
+train_labels = list(map(lambda a: int(a), train_labels))
+
+# print(train_labels)
+    
+# more stuff to save
+
+def converge_to_binary(x):
+    x = np.where(x < .5, -1, 1)
+    return x
+
+def converge_to_prob(x):
+    x = np.where(x < 0., 0., 1.)
+    return x
+
+# Defining input size, hidden layer size, output size and batch size respectively
+n_in, n_h, n_out, hidden_layers, activation_function = 50, 10, 1, 1, 'lstm'
+
+x = torch.tensor(X_train_Glove_s).float()
+y = torch.tensor(train_labels).float()
+
+class MyModule(nn.Module):
+    def __init__(self, 
+            n_in, 
+            neurons_per_hidden, 
+            n_out,
+            hidden_layers, 
+            activation_function,
+            embeddings
+        ):
+
+        super(MyModule, self).__init__()
+
+        self.n_in = n_in
+        self.n_h = neurons_per_hidden
+        self.n_out = n_out
+        self.h_l = hidden_layers
+
+        self.a_f = activation_function
+
+        self.embeddings = embeddings
+
+        # Inputs to hidden layer linear transformation
+        self.input = nn.Linear(n_in, self.n_h)
+
+        # Embedding layer
+        self.embedding_layer = nn.Embedding(num_embeddings=len(self.embeddings), embedding_dim=n_in)
+        # Load weights into embedding layer
+        self.embedding_layer.load_state_dict({'weight': self.embeddings})
+        # Do not train embedding layer
+        self.weight.requires_grad = False
+
+        #probably need a variable for recurrent dropout and dropout layer dropout
+
+        # Defaults to Relu if activation_function is improperly sp
+        self.activation_layer = nn.ReLU()
+
+        if activation_function == 'relu':
+            self.activation_layer = nn.ReLU()
+        elif activation_function == 'tanh':
+            self.activation_layer = nn.Tanh()
+        elif activation_function == 'sigmoid':
+            self.activation_layer == nn.Sigmoid()
+        elif activation_function == 'identity':
+            self.activation_layer == nn.Identity()
+        elif activation_function == 'lstm':
+            self.activation_layer == nn.LSTM(bidirectional=True, dropout=0.2)
+        else:
+            print("Invalid activation function specified")
+            sys.exit(1)
+
+        # self.linears = nn.ModuleList([nn.Linear(self.n_h, self.n_h) for i in range(self.h_l - 1)])
+        self.activation_layers = nn.ModuleList([self.activation_layer for i in range(self.h_l - 1)])
+        self.dropout_layers = nn.ModuleList([nn.Dropout(0.5) for i in range(self.h_l - 1)])
+        
+        # Output layer, 10 units - one for each digit
+        # self.output = nn.Linear(self.n_h, n_out)
+        self.output == nn.ReLU()
+        # Define sigmoid output
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+
+        x = self.input(x)
+        x = self.activation_layer(x)
+
+        # ModuleList can act as an iterable, or be indexed using ints
+        for i, l in enumerate(self.linears):
+            # x = self.linears[i // 2](x) + l(x)
+            x = self.activation_layers[i // 2](x) + l(x)
+            x = self.dropout_layers[i // 2](x) + l(x)
+
+        x = self.output(x)
+        x = self.sigmoid(x)
+
+        return x
+
+embeddings = torch.tensor(embeddings_dict_s)
+model = nn.Sequential(MyModule(n_in, n_h, n_out, hidden_layers, activation_function, embeddings))
+print(model)
+
+# Construct the loss function
+criterion = torch.nn.BCELoss()
+# Construct the optimizer (Adam in this case)
+optimizer = torch.optim.Adam(model.parameters(), lr = 0.05)
+
+# Optimization
+for epoch in range(50):
+   # Forward pass: Compute predicted y by passing x to the model
+   y_pred = model(x)
+
+   y_pred_numpy = y_pred.detach().numpy()
+   y_pred_tanh_range = converge_to_binary(y_pred_numpy)
+   y_numpy = y.detach().numpy()
+#    y_sigmoid_range = converge_to_prob(y_numpy)
+
+#    print(sklearn.metrics.accuracy_score(y_pred_numpy, y))
+
+   # Compute and print loss
+   loss = criterion(y_pred, torch.tensor(y_numpy).float())
+#    print('epoch: ', epoch,' loss: ', loss.item())
+
+   # Zero gradients, perform a backward pass, and update the weights.
+   optimizer.zero_grad()
+
+   # perform a backward pass (backpropagation)
+   loss.backward()
+
+   # Update the parameters
+   optimizer.step()
+
+# Plotting training data
+# plot_data(X, Y)
+
+y_pred_tanh_range_from_train = y_pred_tanh_range
+
+y_pred = model(x_test)
+y_pred_numpy = y_pred.detach().numpy()
+y_pred_tanh_range = converge_to_binary(y_pred_numpy)
+
+# print("Training Accuracy")
+# print(sklearn.metrics.accuracy_score(y_pred_tanh_range_from_train, y))
+
+# print("Testing Accuracy")
+# print(sklearn.metrics.accuracy_score(y_pred_tanh_range, y_test))
+
+# Plotting the test data
+# plot_data(x_test, y_pred_tanh_range)
+
